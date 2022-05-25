1f83a47db6c4fcf107700dfafeef4cd4a7ada3d1
schan <105832330+fullspringwater@users.noreply.github.com>
2022-05-25 14:37:25 +0900

    update

--- app_divorce_test.py
+++ app_divorce_test.py
@@ -8,11 +8,13 @@ def run_divorce_test() :
 
     scaler = joblib.load('data/scaler.pkl')
     classifier = joblib.load('data/classifier.pkl')
+    
     st.title('이혼 가능성 테스트')
     st.subheader('각 테스트 문항은 0 - 4 점으로 이루어져 있습니다')
     st.subheader('0 = 전혀 그렇지 않다, 1 = 약간 그렇지 않다, 2 = 보통이다')
     st.subheader('3 = 약간 그렇다, 4 = 항상 그렇다.')
 
+    # 54개의 라디오를 만들고 버튼을 누를시 결과를 보여준다.
     check_list = np.arange(54)
 
     for i in range(54) :
--- app_eda.py
+++ app_eda.py
@@ -11,36 +11,37 @@ def run_eda() :
 
     q = pd.read_csv('data/question.csv')
     
-    st.subheader('이혼 데이터와 이혼하지 않은 데이터의 갯수와 비율')
+    st.subheader('이혼 데이터와 이혼하지 않은 데이터의 개수와 비율')
     fig1 = plt.figure(figsize=(6,6))
 
+    # 이혼 데이터와 이혼하지 않은 데이터의 개수 비교
     plt.subplot(1,3,1)    
     sns.countplot(data = df, x = 'Divorce')   
 
+
+
+    # 이혼 데이터와 이혼하지 않은 데이터의 비율
     divorce_rate = df['Divorce'].value_counts()
     divorce_rate.rename( index = {0 : 'Not Divorce',
                               1 : 'Divorce'} , inplace = True)
-   
     plt.subplot(1,3,3)
     plt.pie(divorce_rate, autopct = '%.1f', labels = divorce_rate.index,
        startangle = 90, wedgeprops= {'width' : 0.7})
     st.pyplot(fig1)
 
+    # question의 분항들을 받아온다
     col_list= []
     for i in range(1,54+1) :
         col_list.append('Q' + str(i))
     
-    # col_choice = st.selectbox('문항의 내용 보기', col_list)
-
-    # if len(col_choice) != 0 :
-    #     q_context = q.loc[q['Question'] == col_choice, 'context'].values[0]
-    #     st.subheader(col_choice + ' : '+ q_context)
+    # 선택한 문항의 내용을 보여준다.
     col_choice = st.multiselect('문항의 내용 보기', col_list)
     if len(col_choice) != 0 :
         for i in range(len(col_choice)) :
             q_context = q.loc[q['Question'] == col_choice[i], 'context'].values[0]
             st.subheader(col_choice[i] + ' : '+ q_context)
 
+    # 두개 문항을 선택시 regplot과 상관관계 보여준다.
     choice_corr_list = st.multiselect('두개 문항의 상관관계 보기', col_list)
     if st.button('결과보기') :
         if len(choice_corr_list) == 2:
