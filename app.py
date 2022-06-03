97e6de5793686d7c3c3481a4ac03d1e21f42a762
schan <105832330+fullspringwater@users.noreply.github.com>
2022-06-03 09:45:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ import streamlit as st
 from app_divorce_test import run_divorce_test
 from app_eda import run_eda
 from app_home import run_home
+from streamlit_option_menu import option_menu
 
 def main() : 
     menu = ['Home', 'EDA', 'Divorce Test']
-    menu_choice = st.sidebar.selectbox('메뉴', menu)
 
+    with st.sidebar:
+        menu_choice = option_menu("Menu", menu,
+                            icons=['house', 'graph-up', 'book'],
+                            menu_icon="app-indicator", default_index=0,
+                            styles={
+            "container": {"padding": "5!important", "background-color": "#44475A5"},
+            "icon": {"color": "orange", "font-size": "25px"}, 
+            "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#BD93F9"},
+            "nav-link-selected": {"background-color": "#02ab21"},
+        }
+        )
+    
     if menu_choice == menu[0] :
         run_home()
     if menu_choice == menu[1] :
