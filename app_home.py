3b353db6f68d7f5d04bb3871740e1832a60e1ba7
schan <105832330+fullspringwater@users.noreply.github.com>
2022-06-07 15:28:58 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import streamlit as st
 from PIL import Image
 
@@ -6,7 +5,7 @@ def run_home() :
     logo = Image.open('data/logo.jpg')
     st.image(logo)
 
-    
+
     st.title('이혼 가능성 예측하기 프로젝트')
     st.subheader('왼쪽 메뉴의 EDA')
     st.text('테스트를 위해 사용한 데이터에 관한 분석을 볼 수 있습니다.')
